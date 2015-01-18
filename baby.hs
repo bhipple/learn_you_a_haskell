22f5f2c7ee65a069f5465bb7e57bc36d075e4bc8
Benjamin Hipple <bhipple@bloomberg.net>
2015-01-18 17:58:00 -0500

    function syntax

--- baby.hs
+++ baby.hs
@@ -8,4 +8,22 @@ doubleSmallNumber x = if x > 100
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
-let a = [x*y | x <- [2,5,10], y <- [8, 10, 11]]
+a = [x*y | x <- [2,5,10], y <- [8, 10, 11]]
+
+removesUpperCase str = [c | c <- str, c `elem` ['a'..'z']]
+
+-- Alternate ways of doing factorial:
+factorial 0 = 1
+factorial x = x * factorial (x-1)
+fact x = product [1..x]
+
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+-- Let binding can go into the function like this
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+-- Case expressions
+describeList xs = "The list is " ++ case xs of [] -> "empty"
+                                               _:[] -> "a single element"
+                                               x:y:_ -> "long"

a9fc99c48bc4a3345d63f05d00af259cc7ed48a8
Benjamin Hipple <bhipple@bloomberg.net>
2015-01-18 02:17:05 -0500

    lists

--- baby.hs
+++ baby.hs
@@ -5,3 +5,7 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+let a = [x*y | x <- [2,5,10], y <- [8, 10, 11]]
